b711ef3a5423ae0ab183dd9705232c7d4d21abaf
mac-00015 <shail.patel@mindinventory.com>
2020-02-06 10:40:03 +0530

    Initial Commit

---
+++ MIOTPVerification/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MIOTPVerification
+//
+//  Created by mac-00015 on 06/02/20.
+//  Copyright © 2020 mac-00015. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
